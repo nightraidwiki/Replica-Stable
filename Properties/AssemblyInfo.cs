666c0bb72c072b340c0487278f440435099949ab
nightraidwiki <nightraidwiki@users.noreply.github.com>
2026-08-23 18:07:14 +0200

    Release v1.0.6.81

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,13 +8,14 @@ using System.Security.Permissions;
 [assembly: AssemblyCompany("Replica")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyDescription("Replicate few things")]
-[assembly: AssemblyFileVersion("1.0.6.80")]
-[assembly: AssemblyInformationalVersion("1.0.6.80")]
+[assembly: AssemblyFileVersion("1.0.6.81")]
+[assembly: AssemblyInformationalVersion("1.0.6.81")]
 [assembly: AssemblyProduct("Replica")]
 [assembly: AssemblyTitle("Replica")]
 [assembly: TargetPlatform("Windows7.0")]
 [assembly: SupportedOSPlatform("Windows7.0")]
-[assembly: AssemblyVersion("1.0.6.80")]
+[assembly: AssemblyVersion("1.0.6.81")]
+
 
 
 
